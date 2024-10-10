f8e5db4796e3815e6d9635356b8e9f8b02211404
Thai Tran <thai.tommy.tran@gmail.com>
2024-10-10 15:35:50 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      cyan: "#8BE9FD",
+      green: "#50FA7B",
+      orange: "##FFB86C",
+      pink: "##FF79C6",
+      purple: "##BD93F9",
+      red: "###FF5555",
+      yellow: "##F1FA8C"
+    },
+    extend: {}
+  },
+  plugins: []
+};
