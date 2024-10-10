b8c688c1991407f15b3bb4691d04d2260d8b96e1
Thai Tran <thai.tommy.tran@gmail.com>
2024-10-10 15:52:08 -0500

    configured setup files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      cyan: "#8BE9FD",
-      green: "#50FA7B",
-      orange: "##FFB86C",
-      pink: "##FF79C6",
-      purple: "##BD93F9",
-      red: "###FF5555",
-      yellow: "##F1FA8C"
-    },
     extend: {}
   },
   plugins: []
