0b6b09561929fb120a595f363082a3a20fb027c9
Thai Tran <thai.tommy.tran@gmail.com>
2024-10-17 21:58:58 -0500

    added static SoundWave component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import CircularGauge from "./components/CircularGauge";
-import DecibelStats from './components/DecibelStats';
-import RecordButton from './components/RecordButton';
+import DecibelStats from "./components/DecibelStats";
+import RecordButton from "./components/RecordButton";
+import SoundWave from "./components/SoundWave";
 
 function App() {
   return (
     <div className="flex flex-col h-screen bg-gray-900 p-6 overflow-hidden items-center">
       {/* Solid background color */}
-      <div className="absolute inset-0 bg-[#282a36] opacity-90" /> 
+      <div className="absolute inset-0 bg-[#282a36] opacity-90" />
       {/* Gradient background overlay */}
       <div className="absolute inset-0 bg-gradient-to-br from-purple-900/30 to-cyan-900/30" />
       {/* Card container */}
-      <div className="relative z-10 flex flex-col h-full rounded-lg border bg-gray-800/40 border-purple-500 py-6 px-4 min-w-[460px]">
+      <div className="relative z-10 flex flex-col h-full rounded-lg border bg-blue-800/15 border-purple-500 py-6 px-4 min-w-[460px] max-w-[620px] w-full">
         {/* App header and title */}
         <header className="text-center mb-8">
           <h1 className="text-4xl font-bold bg-gradient-to-r from-pink-400 via-purple-400 to-cyan-400 text-transparent bg-clip-text">
@@ -20,6 +21,7 @@ function App() {
         <div className="flex-grow flex flex-col items-center justify-center space-y-12">
           <CircularGauge />
           <DecibelStats />
+          <SoundWave />
           <RecordButton />
         </div>
       </div>
---
+++ src/components/SoundWave.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const SoundWave = () => {
+  return (
+    <div className="flex items-center justify-between h-40 w-full bg-gray-900 border-purple-500 border rounded-md overflow-hidden">
+      {[...Array(60)].map((_, index) => {
+        const height = Math.random() * (90 - 10) + 10;
+
+        return (
+          <div key={index} className="flex flex-col justify-center h-full">
+            <div
+              className="w-1 bg-gradient-to-t from-pink-500 to-purple-400 rounded-sm transition-all duration-75 ease-in-out"
+              style={{ height: `${height / 2}%` }}
+            ></div>
+            <div
+              className="w-1 bg-gradient-to-t from-purple-400 to-pink-500 rounded-sm transition-all duration-75 ease-in-out"
+              style={{ height: `${height / 2}%` }}
+            ></div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default SoundWave;
